2ac0e3c6a2130506c42075d2ddb208c2a3fbda43
Palladin4ik <64522831+Palladin4ik@users.noreply.github.com>
2020-11-12 23:28:31 +0300

    Добавил Bootstrap

--- not-bad-cook/src/App.vue
+++ not-bad-cook/src/App.vue
@@ -4,3 +4,11 @@
   </div>
 </template>
 
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  
+}
+</script>
--- not-bad-cook/src/views/Home.vue
+++ not-bad-cook/src/views/Home.vue
@@ -1,6 +1,3 @@
 <template>
-  <div class="home">
 
-  </div>
 </template>
-

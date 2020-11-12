4d01579131ae4176cf620df31bc34923c91e4802
Palladin4ik <64522831+Palladin4ik@users.noreply.github.com>
2020-11-12 23:42:19 +0300

    Добавил иконки от Bootstrap

--- not-bad-cook/src/main.js
+++ not-bad-cook/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
